0c2d213d8fbb17853719d7363424cc16069ad880
Rachit Ahuja <rachitahuja20@gmail.com>
2017-04-12 09:37:43 +0000

    Initialize repository

--- CityProject.rb
+++ CityProject.rb
@@ -1,11 +1,24 @@
 
-city = { "Delhi" => '011', "Mumbai" => '012', "Jaipur" => '013', "Chandigarh" => '014', "Punjab" => '015', "Gujrat" => '016', "Surat" => '017', "Pune" => '018', "Orissa" => '019', "Bihar" => '020'}
-          
+city = { 
+  "Delhi" => '011', 
+  "Mumbai" => '012', 
+  "Jaipur" => '013', 
+  "Chandigarh" => '014', 
+  "Punjab" => '015', 
+  "Gujrat" => '016', 
+  "Surat" => '017', 
+  "Pune" => '018', 
+  "Orissa" => '019', 
+  "Bihar" => '020'
+  }
+     
+# Method to dispay the city name          
 def display_city(city)
   city.each { |k,v| puts k }
 end
 
-def loookUp(name, city)
+# Method to lookup the city name  
+def lookUp(name, city)
   
   if city.has_key?(name) == TRUE
     puts "The city code is #{city[name]}"
@@ -21,11 +34,10 @@ puts "Which city code do you want, please choose onec city out of the following"
 display_city(city)
 
 begin 
-  
   puts "Enter the name of city"
   ocity = gets.chomp
   
-  loookUp(ocity, city)
+  lookUp(ocity, city)
   
   puts "Do you want to continue?(Y/N)"
   ch = gets.chomp
